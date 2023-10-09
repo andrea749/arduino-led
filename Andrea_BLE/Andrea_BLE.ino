6ba0f60d9a4b15c037a57e945003a4abe0d4841a
Andrea Garcia <andreagarcia749@gmail.com>
2023-10-09 19:04:53 -0400

    clean up comments and some code

--- Andrea_BLE/Andrea_BLE.ino
+++ Andrea_BLE/Andrea_BLE.ino
@@ -15,22 +15,11 @@ using namespace std;
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
-// vector<vector<float>> song_vec;
-// #include <json.hpp>
-// using json = nlohmann::json;
-
-// typedef unsigned char BYTE;
-// using namespace nlohmann::literals;
 
 int buttonPin = 2;
-// boolean ledSwitch;
 
-BLEService LEDService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
-// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
-// BLEByteCharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite);
-// BLEStringCharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite, 512);
+BLEService LEDService("19B10000-E8F2-537E-4F6C-D104768A1214"); 
 BLEStringCharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify | BLEWrite, 512);
-// array<array<float>> song_arr;
 StaticJsonDocument<3072> doc;
 
 
@@ -61,7 +50,6 @@ void loop() {
   // listen for BLE peripherals to connect:
   BLEDevice central = BLE.central();
   String songJson = "";
-  // if a central is connected to peripheral:
   if (central) {
     Serial.print("Connected to central: ");
     // print the central's MAC address:
@@ -70,48 +58,27 @@ void loop() {
     
     while (central.connected()) {
       if (LEDCharacteristic.written()) {
-        Serial.println("char written, new val:: ");
         String value = LEDCharacteristic.value();
         songJson += value;
-        Serial.println(value);
-        Serial.println(songJson);
-        // std::cout << s << std::endl;
       }
     }
   // }
   Serial.print(F("Disconnected from central: "));
   Serial.println(central.address());
-  Serial.println(songJson);
-  Serial.println("lasdlajsknjhjhjhjjhjhjhjhjhdl");
   DeserializationError error = deserializeJson(doc, songJson);
   if (error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
   }
-  Serial.println("lasdlajsdl");
   int tempo = doc["tempo"]; // 120
-  // int numEvents = doc["numEvents"]; // idk
-  // Serial.println("numEvents");
-  // Serial.println(numEvents);
   JsonArray notes = doc["notes"];
-  Serial.println("notes.size()");
-  Serial.println(notes.size());
-  int numEvents = notes.size(); // idk
+  int numEvents = notes.size();
   float song[numEvents][3]; // 2d array representing whole song. numEvents # of subarrays and each subarray has 3 things (time, pitch, on/off)
+  // convert everything to floats
   for (int i = 0; i < numEvents; i++) {
-    float currEvent[3];
     for (int j = 0; j < 3; j++) {
-      currEvent[j] = std::atof(notes[i][j]);
-      Serial.print("curr num/note:: ");
-      Serial.println(currEvent[j]);
       song[i][j] = std::atof(notes[i][j]);
-    }
-    // std::memcpy(song[i], currEvent, 3);
-    for (int k=0;k<3;k++) {
-
-      Serial.println("song[i][k]");
-      Serial.println(song[i][k]);
     }
   }
   // }
@@ -125,36 +92,20 @@ void loop() {
     }
     // iterate through all notes of same time stamp and set LED
     for (int i=note_window_start; i<note_window_end; i++) { // i = 0, end = 1, start = 0
-      // vector<float> curr_note = song[i]; // {0.0, 2.0, 1.0}
-      float curr_note[3] = {*song[i]}; // {0.0, 2.0, 1.0}
-      for (int j=0;j<3;j++) {
-        Serial.println("curr_note[j]");
-        Serial.println(curr_note[j]);
-        Serial.println("song[i][j]");
-        Serial.println(song[i][j]);
-
-      }
       int pitch = (int)song[i][1];
-      Serial.println("pitch");
-      Serial.println(pitch);
-      Serial.println("pitch222");
-      Serial.println(song[i][1]);
       if (pitch < 0) {
         pitch *= -1;
       }
       // Black == LED off
       leds[pitch] = (song[i][2] == 0.0) ? CRGB::Black : CRGB::Purple;
     }
-    // set delay, subtract timestamp of current window from timestamp of next window
-    // error: if last note is at the same time as prev note, we might not clear the LEDs at the same time. potentially doesn't matter
-    // float delay = song_vec[note_window_end][0] - song_vec[note_window_end-1][0]; // replace w/start
+
     FastLED.show();
     // 1 tick every 2 seconds (1/8th note per second, approx 30bpm?) = magnitude of 2000
     // FastLED.delay(delay*2000);
     FastLED.delay(2000);
     // slide window down past processed lights. not sure if subscripting error might happen with moving window past end of song
     if (note_window_end < numEvents-1) {
-      // Serial.println("move window");
       note_window_start = note_window_end;
       note_window_end = note_window_start + 1;
     } else {
@@ -164,13 +115,14 @@ void loop() {
     // one final clear just in case
     FastLED.clear();
   }
+  // for debugging purposes
   leds[40] = CRGB::Blue;
   leds[35] = CRGB::Blue;
   leds[30] = CRGB::Blue;
   leds[25] = CRGB::Blue;
   leds[20] = CRGB::Blue;
 }
-leds[40] = CRGB::Blue;
+  leds[40] = CRGB::Blue;
   leds[35] = CRGB::Blue;
   leds[30] = CRGB::Blue;
   leds[25] = CRGB::Blue;
